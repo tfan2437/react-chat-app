6fe8c0d110c233973cba2ed1728c991d88175949
tfan2437 <tfan2437@gmail.com>
2024-06-08 04:05:58 -0400

    basic chat layout complete

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
 import "./Chat.css";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
   const [emojiMenu, setEmojiMenu] = useState(false);
   const [message, setMessage] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -31,10 +37,16 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
+
       <div className="chat-center">
-        <div className="message">
+        <div className="message-others">
           <img src="./avatar.png" alt="" />
-          <div className="texts">
+          <div className="message-text">
+            <img
+              src="https://static.miraheze.org/hololivewiki/thumb/a/a0/Momosuzu_Nene_-_Portrait_02.png/580px-Momosuzu_Nene_-_Portrait_02.png"
+              alt=""
+              className="attached-image"
+            />
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
               rem minus assumenda, animi ducimus deleniti ipsum corporis,
@@ -44,6 +56,66 @@ const Chat = () => {
             <span>1 min ago</span>
           </div>
         </div>
+        <div className="message-own">
+          <div className="message-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              rem minus assumenda, animi ducimus deleniti ipsum corporis,
+              dolorem tempora id, iste culpa? Accusamus natus officia doloribus
+              eius accusantium ullam iure.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message-others">
+          <img src="./avatar.png" alt="" />
+          <div className="message-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              rem minus assumenda, animi ducimus deleniti ipsum corporis,
+              dolorem tempora id, iste culpa? Accusamus natus officia doloribus
+              eius accusantium ullam iure.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message-own">
+          <div className="message-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              rem minus assumenda, animi ducimus deleniti ipsum corporis,
+              dolorem tempora id, iste culpa? Accusamus natus officia doloribus
+              eius accusantium ullam iure.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message-others">
+          <img src="./avatar.png" alt="" />
+          <div className="message-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              rem minus assumenda, animi ducimus deleniti ipsum corporis,
+              dolorem tempora id, iste culpa? Accusamus natus officia doloribus
+              eius accusantium ullam iure.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message-own">
+          <div className="message-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              rem minus assumenda, animi ducimus deleniti ipsum corporis,
+              dolorem tempora id, iste culpa? Accusamus natus officia doloribus
+              eius accusantium ullam iure.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div ref={endRef}></div>
       </div>
       <div className="chat-bottom">
         <div className="icons">
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,60 @@
 import "./Detail.css";
 
 const Detail = () => {
-  return <div className="detail-container">Detail</div>;
+  return (
+    <div className="detail-container">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+
+        <div className="photo">
+          <div className="photo-item">
+            <div className="photo-detail">
+              <img
+                src="https://viberatecdn.blob.core.windows.net/entity/artist/momosuzu-nene-tao-ling-nene-7HJZV"
+                alt=""
+              />
+              <span>photo_2024_5.png</span>
+            </div>
+            <img src="./download.png" alt="" className="download-icon" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <button className="block-user">Block User</button>
+        <button className="log-out">Log Out</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
